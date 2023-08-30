d596a9d54b5247a8a9db13c6373b5285fd152f19
Thiago Jose Gontijo Cardoso <thiago.gontijo@mj.gov.br>
2023-08-30 12:51:55 -0300

    Alter page to Instituitions

---
+++ src/app/instituition/page.tsx
@@ -0,0 +1,68 @@
+"use client"
+"use client"
+
+import { useEffect, useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+interface Instituition {
+  id: string,
+  short_name: string,
+  name: string
+}
+
+export default function Instituition() {
+
+  const [instituitions, setInstituitions] = useState<Instituition[]>([]); // Fix the variable name here
+
+  async function fetchInstituitions() { // Fix the function name here
+    try {
+      const response = await fetch("http://localhost:3333/instituition", { cache: 'force-cache' });
+      const data = await response.json();
+      console.log("Dados da API:", data);
+
+      if (Array.isArray(data.instituition)) {
+        setInstituitions(data.instituition);
+      } else {
+        console.error("Erro ao buscar áreas temáticas.");
+      }
+    } catch (error) {
+      console.error("Erro ao buscar áreas temáticas:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchInstituitions();
+  }, []);
+
+  return (
+    <>
+      <div className="pt-8">
+        <h1>Instituições</h1>
+        <Table>          
+          <TableHeader>
+            <TableRow>
+              <TableHead>Sigla</TableHead>
+              <TableHead>Nome</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {instituitions.map((instituition) => (
+              <TableRow key={instituition.id}>
+                <TableCell>{instituition.short_name}</TableCell>
+                <TableCell>{instituition.name}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </>
+  )
+}
