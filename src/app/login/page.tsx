df718ea3c6209537c215201dab7fccc13676ff10
Thiago Jose Gontijo Cardoso <thiago.gontijo@mj.gov.br>
2023-09-11 17:57:30 -0300

    Create login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,99 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import * as z from "zod"
+import { useForm } from "react-hook-form"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
+const formSchema = z.object(
+  {   
+    email: z.string().email( {
+      message: "Informe um e-mail válido",
+    }),
+    password: z.string().min(6, {
+      message: "Informe uma senha válida",
+    })   
+  }
+)
+
+export default function SignIn() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {     
+      email: "",     
+      password: "",
+    }
+  })
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      const response = await fetch("http://localhost:3333/sessions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      });
+
+      if (response.ok) {
+        form.reset();
+        console.log("Usuário cadastrado com sucesso!");
+      } else {       
+        console.error("Erro ao cadastrar o usuário:", response.status);
+        const errorData = await response.json();
+        console.error("Detalhes do erro:", errorData);
+      }
+    } catch (error) {
+      console.error("Erro na requisição:", error);
+    }
+  }
+  
+
+
+  return (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-96">         
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>E-mail</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"            
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Senha</FormLabel>
+                <FormControl>
+                  <Input {...field} type="password" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />       
+          
+          <Button variant="outline" type="submit">Entrar</Button>
+        </form>
+      </Form>     
+    </>
+  )
+}
