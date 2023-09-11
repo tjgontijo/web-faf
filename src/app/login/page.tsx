480f0b3556e4fb8877f08897b0891a2708620de3
Thiago Jose Gontijo Cardoso <thiago.gontijo@mj.gov.br>
2023-09-11 18:47:57 -0300

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { useEffect, useState } from "react"
 import { zodResolver } from "@hookform/resolvers/zod"
 import * as z from "zod"
 import { useForm } from "react-hook-form"
