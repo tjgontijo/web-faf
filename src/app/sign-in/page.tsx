f3fa5467d6170fa49efdac1b66fd3b8c908940a9
Thiago Jose Gontijo Cardoso <thiago.gontijo@mj.gov.br>
2023-09-14 14:39:38 -0300

    Update page.tsx

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { useEffect, useState } from "react"
 import { zodResolver } from "@hookform/resolvers/zod"
 import * as z from "zod"
 import { useForm } from "react-hook-form"
@@ -12,47 +11,32 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form"
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 
 const formSchema = z.object(
-  {
-    name: z.string().min(1, {
-      message: "Informe um nome válido"
-    }),
+  {   
     email: z.string().email( {
       message: "Informe um e-mail válido",
     }),
     password: z.string().min(6, {
       message: "Informe uma senha válida",
-    }),
-    stateId: z.string().min(1, {
-      message: "Informe um Estado válido",
-    }),
+    })   
   }
 )
 
 export default function SignIn() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: {
-      name: "",
-      email: "",
-      stateId: "",
+    defaultValues: {     
+      email: "",     
       password: "",
     }
   })
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
-      const response = await fetch("http://localhost:3333/users", {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/sessions`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -63,8 +47,7 @@ export default function SignIn() {
       if (response.ok) {
         form.reset();
         console.log("Usuário cadastrado com sucesso!");
-      } else {
-        // Lide com erros na resposta do servidor
+      } else {       
         console.error("Erro ao cadastrar o usuário:", response.status);
         const errorData = await response.json();
         console.error("Detalhes do erro:", errorData);
@@ -73,51 +56,14 @@ export default function SignIn() {
       console.error("Erro na requisição:", error);
     }
   }
+  
 
-  interface State {
-    id: string;
-    email: string;
-    name: string;
-  }
-
-  const [states, setStates] = useState<State[]>([]);
-
-  async function fetchStates() {
-    try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/states`);
-      const data = await response.json();
-      console.log("Dados da API:", data);
-
-      if (Array.isArray(data.states)) {
-        setStates(data.states);
-      } else {
-        console.error("Erro ao buscar áreas temáticas.");
-      }
-    } catch (error) {
-      console.error("Erro ao buscar áreas temáticas:", error);
-    }
-  }
-  useEffect(() => {
-    fetchStates();
-  }, []);
 
   return (
     <>
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-96">
-          <FormField
-            control={form.control}
-            name="name"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Nome</FormLabel>
-                <FormControl>
-                  <Input {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-96">   
+              
           <FormField
             control={form.control}
             name="email"
@@ -143,33 +89,9 @@ export default function SignIn() {
                 <FormMessage />
               </FormItem>
             )}
-          />
-          <FormField
-            control={form.control}
-            name="stateId"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Estado</FormLabel>
-                <Select onValueChange={field.onChange} defaultValue={field.value}>
-                  <FormControl>
-                    <SelectTrigger>
-                      <SelectValue />
-                    </SelectTrigger>
-                  </FormControl>
-                  <SelectContent style={{ maxHeight: '200px', overflowY: 'auto' }}>
-                    {states.map((state) => (
-                      <SelectItem key={state.id} value={state.id}>
-                        {state.name}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+          />       
           
-          <Button variant="outline" type="submit">Cadastrar</Button>
+          <Button variant="outline" type="submit">Entrar</Button>
         </form>
       </Form>     
     </>
