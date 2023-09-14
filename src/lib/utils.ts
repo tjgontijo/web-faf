5c017d35736688aae75be6a68756bba1c215a6b7
Thiago Jose Gontijo Cardoso <thiago.gontijo@mj.gov.br>
2023-09-14 15:51:24 -0300

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import format from 'date-fns/format';
  
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const formatDate = (dateString: string): string => {
+  return format(new Date(dateString), 'dd/MM/yyyy HH:mm');
 }
