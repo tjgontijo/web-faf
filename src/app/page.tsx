0287d9915c1924b943d156758eba974af2e54a21
Thiago Gontijo <tjgontijo@gmail.com>
2023-09-23 20:48:29 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,8 @@
-"use client"
-import GoogleBarChart from '@/components/chart/AreaChart';
-
 export default function Home() {
-
-  const labels = ['AC', 'AL', 'AP', 'AM', 'BA'];
-  const data = [12, 19, 3, 5, 2];
 
   return (
     <div>
-      <h1>Painel de Acompanhamento</h1>
-      <div className="flex gap-2">
-        <div className="flex">
-            <GoogleBarChart labels={labels} data={data} title='Ações por Estado' />
-        </div>
-        <div className="flex">
-            <GoogleBarChart labels={labels} data={data} title='Ações por Natureza de Despesa' />
-        </div>
-      </div>
-      
-      
+      <p>Login</p>
     </div>
   );
 }
