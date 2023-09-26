f452d2f7e3382faee328f9ed4404747c1eaf90fd
Thiago Jose Gontijo Cardoso <thiago.gontijo@mj.gov.br>
2023-09-26 13:54:33 -0300

    Add link to dash

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import Link from "next/link";
+
 export default function Home() {
 
   return (
-    <div>
-      <p>Login</p>
-    </div>
+      <div className="flex justify-center h-screen items-center">
+        <Link href="/dashboard">
+          <Button>
+            <Plus className="h-4 w-4 pr-1" /> Entrar
+          </Button>
+        </Link>
+      </div>
   );
 }
